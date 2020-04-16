a60c6d36cf8e2e969927259fc07eca2ca010893c
Nunzia <nunzia.fina@gmail.com>
2020-04-16 14:21:09 +0200

    Creo array di studenti e stampo nome e cognome

--- main.js
+++ main.js
@@ -1,7 +1,4 @@
 
-
-// - Creare un array di oggetti di studenti.
-// -Ciclare su tutti gli studenti e stampare per ognuno nome e cognome;
 // - Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
 
 
@@ -17,4 +14,33 @@ var studente = {
 
 for (var key in studente){
   console.log(key + " " + studente[key]);
+}
+
+// Creare un array di oggetti di studenti
+
+var studenti = [
+  {
+    "nome": "Patrizia",
+    "cognome": "Garganese",
+    "eta": 31
+  },
+  {
+    "nome": "Luigi",
+    "cognome": "Mangia",
+    "eta": 32
+  },
+  {
+    "nome": "Antonio",
+    "cognome": "Spagnolo",
+    "eta": 36
+  }
+];
+
+// Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
+
+for (var i = 0; i < studenti.length; i++){
+  var studente1 = studenti[i];
+  var nomeStudente = studente1.nome;
+  var cognomeStudente = studente1.cognome;
+  console.log(nomeStudente + " " + cognomeStudente);
 }
